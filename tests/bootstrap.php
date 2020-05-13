841fc018b24614a17fe91ea725afde81b84dd835
Jakub Matějka <jakub@keboola.com>
2020-05-13 20:20:43 +0200

    fix: code style

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,18 +1,20 @@
 <?php
+
+declare(strict_types=1);
+
 define('ROOT_PATH', __DIR__);
-ini_set('display_errors', true);
+ini_set('display_errors', '1');
 error_reporting(E_ALL);
 date_default_timezone_set('Europe/Prague');
 
-set_error_handler('exceptions_error_handler');
-function exceptions_error_handler($severity, $message, $filename, $lineno)
-{
-    if (error_reporting() == 0) {
-        return;
+set_error_handler(function (int $severity, string $message, string $filename, int $lineno): bool {
+    if (error_reporting() === 0) {
+        return false;
     }
     if (error_reporting() & $severity) {
         throw new ErrorException($message, 0, $severity, $filename, $lineno);
     }
-}
+    return true;
+});
 
 require_once __DIR__ . '/../vendor/autoload.php';
