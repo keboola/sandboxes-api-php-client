1567de007ad62642a398ca8493002798baaf07bb
Josef Martinec <josef.martinec@keboola.com>
2022-04-27 16:15:38 +0200

    PS-3373 Store project.persistentStorage.k8sStorageClassName

--- src/PersistentStorage.php
+++ src/PersistentStorage.php
@@ -7,15 +7,21 @@ namespace Keboola\Sandboxes\Api;
 class PersistentStorage
 {
     private ?bool $ready = null;
+    private ?string $k8sStorageClassName = '';
 
-    public function __construct()
+    public static function create(): self
     {
+        return new self();
     }
 
     public static function fromArray(array $values): self
     {
-        return (new self())
-            ->setReady($values['ready'] ?? null);
+        return self::create()
+            ->setReady($values['ready'] ?? null)
+            ->setK8sStorageClassName(
+                array_key_exists('k8sStorageClassName', $values) ? $values['k8sStorageClassName'] : ''
+            )
+        ;
     }
 
     public function setReady(?bool $value): self
@@ -24,15 +30,32 @@ class PersistentStorage
         return $this;
     }
 
+    public function setK8sStorageClassName(?string $name): self
+    {
+        $this->k8sStorageClassName = $name;
+        return $this;
+    }
+
     public function toArray(): array
     {
-        return [
+        $data = [
             'ready' => $this->ready,
         ];
+
+        if ($this->k8sStorageClassName !== '') {
+            $data['k8sStorageClassName'] = $this->k8sStorageClassName;
+        }
+
+        return $data;
     }
 
     public function isReady(): ?bool
     {
         return $this->ready;
+    }
+
+    public function getK8sStorageClassName(): ?string
+    {
+        return $this->k8sStorageClassName;
     }
 }
