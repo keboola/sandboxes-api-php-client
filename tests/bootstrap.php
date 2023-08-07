b4e8079cc2590535e91acb84decf359ccfa9d609
MiroCillik <mcillik@gmail.com>
2023-08-07 15:09:17 +0200

    tests: bootstrap verify token

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,9 @@
 
 declare(strict_types=1);
 
+use Keboola\Sandboxes\Api\Exception\ClientException;
+use Keboola\StorageApi\Client as StorageClient;
+
 define('ROOT_PATH', __DIR__);
 ini_set('display_errors', '1');
 error_reporting(E_ALL);
@@ -22,5 +25,29 @@ if (file_exists(__DIR__ . '/../.env')) {
     $dotenv->load();
 }
 $dotenv->required(['API_URL', 'KBC_MANAGE_TOKEN', 'KBC_STORAGE_TOKEN', 'KBC_URL']);
+
+$storageClient = new StorageClient([
+    'url' => (string) getenv('KBC_URL'),
+    'token' => (string) getenv('KBC_STORAGE_TOKEN'),
+]);
+
+try {
+    $tokenInfo = $storageClient->verifyToken();
+} catch (ClientException $e) {
+    throw new RuntimeException(sprintf(
+        'Failed to verify "%s", check ENV variables: %s',
+        'KBC_STORAGE_TOKEN',
+        $e->getMessage()
+    ), 0, $e);
+}
+
+printf(
+    'Authorized as "%s (%s)" to project "%s (%s)" at "%s" stack.' . "\n",
+    $tokenInfo['description'],
+    $tokenInfo['id'],
+    $tokenInfo['owner']['name'],
+    $tokenInfo['owner']['id'],
+    $storageClient->getApiUrl()
+);
 
 require_once __DIR__ . '/../vendor/autoload.php';
